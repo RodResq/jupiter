b2928879b0d29cdfb20bdbd636fb4cabe1d6e16f
RodResq <rodrigoresque@yahoo.com.br>
2021-12-28 01:48:24 -0300

    first commit

---
+++ jupiter-backend/src/test/java/br/com/jupiter/jupiter/JupiterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jupiter.jupiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
