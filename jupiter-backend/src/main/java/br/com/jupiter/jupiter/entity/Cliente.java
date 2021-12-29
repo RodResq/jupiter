acd58e9207bc48cc51ec1017d02fdf659fcd40e1
RodResq <rodrigoresque@yahoo.com.br>
2021-12-29 00:28:49 -0300

    add entity cliente

--- jupiter-backend/src/main/java/br/com/jupiter/jupiter/entity/Cliente.java
+++ jupiter-backend/src/main/java/br/com/jupiter/jupiter/entity/Cliente.java
@@ -1,6 +1,7 @@
 package br.com.jupiter.jupiter.entity;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "")
@@ -10,8 +11,14 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
 
-    @Column(name = "cod_cliente", nullable = false)
+    @Column(name = "cod_cliente", nullable = false, unique = true)
     public String codCliente;
+
+    @Column(name = "data_hora_entrada", nullable = false)
+    public Date dataHoraEntrada;
+
+    @Column(name = "data_hora_saida", nullable = true)
+    public Date dataHoraSaida;
 
     public void setId(Long id) {
         this.id = id;
@@ -19,5 +26,29 @@ public class Cliente {
 
     public Long getId() {
         return id;
+    }
+
+    public String getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(String codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public Date getDataHoraEntrada() {
+        return dataHoraEntrada;
+    }
+
+    public void setDataHoraEntrada(Date dataHoraEntrada) {
+        this.dataHoraEntrada = dataHoraEntrada;
+    }
+
+    public Date getDataHoraSaida() {
+        return dataHoraSaida;
+    }
+
+    public void setDataHoraSaida(Date dataHoraSaida) {
+        this.dataHoraSaida = dataHoraSaida;
     }
 }
